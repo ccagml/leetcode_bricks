28292d8b95ef43dd349c192c62a84ac15271d244
ccagml <ccagml@163.com>
2022-08-25 16:12:19 +0800

    move shell

--- move_to_dir.sh
+++ move_to_dir.sh
@@ -1,1 +1,24 @@
-#/bin/bash
+
+for file in `ls | grep question`
+do
+    if test -f $file
+    then
+        if [ $1 == "s" ]; 
+        then
+            
+            mv $file "simple"
+        elif [ $1 == "s2" ]; 
+        then
+            
+            mv $file "simple_2"
+        elif [ $1 == "m" ]; 
+        then
+            
+            mv $file "medium"
+        elif [ $1 == "h" ]; 
+        then
+            
+            mv $file "hard"
+        fi
+    fi
+done
