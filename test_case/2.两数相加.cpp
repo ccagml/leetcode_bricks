ca5b1327b1b5d2a1784ba322648fcfe4f67351c4
ccagml <ccagml@163.com>
2022-11-11 02:27:07 +0800

    update

---
+++ test_case/2.两数相加.cpp
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=2 lang=cpp
+ *
+ * [2] 两数相加
+ *
+ * https://leetcode.cn/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (42.18%)
+ * Likes:    8883
+ * Dislikes: 0
+ * Total Accepted:    1.6M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ *
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 每个链表中的节点数在范围 [1, 100] 内
+ * 0 <= Node.val <= 9
+ * 题目数据保证列表表示的数字不含前导零
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        if (l1 == NULL and l2 == NULL)
+            return NULL;
+        else if (l1 == NULL)
+            return l2;
+        else if (l2 == NULL)
+            return l1;
+
+        int a = l1->val + l2->val;
+        ListNode *p = new ListNode(a % 10);
+        p->next = addTwoNumbers(l1->next, l2->next);
+        if (a >= 10)
+            p->next = addTwoNumbers(p->next, new ListNode(1));
+        return p;
+    }
+};
+// @lc code=end
