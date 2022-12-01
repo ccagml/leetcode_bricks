9fb50594186a6af9ff79e85e6c63680a3667a0c9
ccagml <ccagml@163.com>
2022-12-01 03:09:43 -0800

    update

--- 416.分割等和子集.cpp
+++ 416.分割等和子集.cpp
@@ -15,42 +15,63 @@
  * Testcase Example:  '[1,5,11,5]'
  *
  * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
+ *
  * 输入：nums = [1,5,11,5]
  * 输出：true
  * 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
- * 
+ *
  * 示例 2：
- * 
+ *
  * 输入：nums = [1,2,3,5]
  * 输出：false
  * 解释：数组不能分割成两个元素和相等的子集。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 200
  * 1 <= nums[i] <= 100
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool canPartition(vector<int>& nums) {
+    bool canPartition(vector<int> &nums)
+    {
+        int target = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            target += nums[i];
+        }
+        if (target % 2 == 1)
+        {
+            return false;
+        }
+        target /= 2;
+        vector<bool> temp(target + 1, false);
+        temp[0] = true;
+        for (int aaa : nums)
+        {
+            for (int j = target; j >= aaa; j--)
+            {
+                temp[j] = temp[j] || temp[j - aaa];
+            }
+        }
 
+        return temp[target];
     }
 };
 // @lc code=end
-
 
 /*
 // @lcpr case=start
@@ -62,5 +83,3 @@ public:
 // @lcpr case=end
 
  */
-
-
