704fac9650c1d202759d63d07a4eb3157f10181d
ccagml <ccagml@163.com>
2022-12-01 03:41:46 -0800

    update

---
+++ 494.目标和.cpp
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode.cn id=494 lang=cpp
+ * @lcpr version=20602
+ *
+ * [494] 目标和
+ *
+ * https://leetcode.cn/problems/target-sum/description/
+ *
+ * algorithms
+ * Medium (49.09%)
+ * Likes:    1448
+ * Dislikes: 0
+ * Total Accepted:    297.2K
+ * Total Submissions: 605.5K
+ * Testcase Example:  '[1,1,1,1,1]\n3'
+ *
+ * 给你一个整数数组 nums 和一个整数 target 。
+ *
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ *
+ *
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ *
+ *
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,1,1,1,1], target = 3
+ * 输出：5
+ * 解释：一共有 5 种方法让最终目标和为 3 。
+ * -1 + 1 + 1 + 1 + 1 = 3
+ * +1 - 1 + 1 + 1 + 1 = 3
+ * +1 + 1 - 1 + 1 + 1 = 3
+ * +1 + 1 + 1 - 1 + 1 = 3
+ * +1 + 1 + 1 + 1 - 1 = 3
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [1], target = 1
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 20
+ * 0 <= nums[i] <= 1000
+ * 0 <= sum(nums[i]) <= 1000
+ * -1000 <= target <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+
+// 前i个凑成j 的数量 x
+class Solution
+{
+public:
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        target = abs(target);
+        // 正的合 + 负的合 = 总数
+        // 正的合 - 负的合 = target
+        // 正的合 = (总数 + target) / 2
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if ((sum + target) % 2 != 0 || sum < target)
+        {
+            return 0;
+        }
+        int need = (sum + target) / 2;
+        vector<int> vi(need + 1);
+        // std::cout << "需要凑出:" << need;
+        vi[0]++;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            for (int j = need; j >= num; j--)
+            {
+                vi[j] += vi[j - num];
+            }
+        }
+        return vi[need];
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,1,1,1,1]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+ */
