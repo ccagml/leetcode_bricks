721409dee86e45b22a8065aceb46bdbd1b404bec
ccagml <ccagml@163.com>
2023-01-19 17:18:50 +0800

    想不出来

---
+++ 1458.两个子序列的最大点积.cpp
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode.cn id=1458 lang=cpp
+ * @lcpr version=21113
+ *
+ * [1458] 两个子序列的最大点积
+ *
+ * https://leetcode.cn/problems/max-dot-product-of-two-subsequences/description/
+ *
+ * algorithms
+ * Hard (45.96%)
+ * Likes:    75
+ * Dislikes: 0
+ * Total Accepted:    7.1K
+ * Total Submissions: 15.4K
+ * Testcase Example:  '[2,1,-2,5]\r\n[3,0,-6]\r'
+ *
+ * 给你两个数组 nums1 和 nums2 。
+ *
+ * 请你返回 nums1 和 nums2 中两个长度相同的 非空 子序列的最大点积。
+ *
+ * 数组的非空子序列是通过删除原数组中某些元素（可能一个也不删除）后剩余数字组成的序列，但不能改变数字间相对顺序。比方说，[2,3,5] 是
+ * [1,2,3,4,5] 的一个子序列而 [1,5,3] 不是。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [2,1,-2,5], nums2 = [3,0,-6]
+ * 输出：18
+ * 解释：从 nums1 中得到子序列 [2,-2] ，从 nums2 中得到子序列 [3,-6] 。
+ * 它们的点积为 (2*3 + (-2)*(-6)) = 18 。
+ *
+ * 示例 2：
+ *
+ * 输入：nums1 = [3,-2], nums2 = [2,-6,7]
+ * 输出：21
+ * 解释：从 nums1 中得到子序列 [3] ，从 nums2 中得到子序列 [7] 。
+ * 它们的点积为 (3*7) = 21 。
+ *
+ * 示例 3：
+ *
+ * 输入：nums1 = [-1,-1], nums2 = [1,1]
+ * 输出：-1
+ * 解释：从 nums1 中得到子序列 [-1] ，从 nums2 中得到子序列 [1] 。
+ * 它们的点积为 -1 。
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums1.length, nums2.length <= 500
+ * -1000 <= nums1[i], nums2[i] <= 100
+ *
+ *
+ *
+ *
+ * 点积：
+ *
+ * 定义 a = [a1, a2,…, an] 和 b = [b1, b2,…, bn] 的点积为：
+ *
+ *
+ *
+ * 这里的 Σ 指示总和符号。
+ *
+ *
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int maxDotProduct(vector<int> &nums1, vector<int> &nums2)
+    {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<vector<int>> f(m, vector<int>(n));
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                int xij = nums1[i] * nums2[j];
+                f[i][j] = xij;
+                if (i > 0)
+                {
+                    f[i][j] = max(f[i][j], f[i - 1][j]);
+                }
+                if (j > 0)
+                {
+                    f[i][j] = max(f[i][j], f[i][j - 1]);
+                }
+                if (i > 0 && j > 0)
+                {
+                    f[i][j] = max(f[i][j], f[i - 1][j - 1] + xij);
+                }
+            }
+        }
+
+        return f[m - 1][n - 1];
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [2,1,-2,5]\n[3,0,-6]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,-2]\n[2,-6,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,-1]\n[1,1]\n
+// @lcpr case=end
+
+ */
