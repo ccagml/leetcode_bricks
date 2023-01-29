d82dc4fe11f4938e3cfb2bd581512b5a75bb3182
ccagml <ccagml@163.com>
2023-01-29 12:29:19 +0800

    重做

---
+++ 10.正则表达式匹配_20230129.cpp
@@ -0,0 +1,118 @@
+/*
+ * @lc app=leetcode.cn id=10 lang=cpp
+ * @lcpr version=21301
+ *
+ * [10] 正则表达式匹配
+ *
+ * https://leetcode.cn/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (30.97%)
+ * Likes:    3408
+ * Dislikes: 0
+ * Total Accepted:    340.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ *
+ *
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ *
+ *
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "aa", p = "a"
+ * 输出：false
+ * 解释："a" 无法匹配 "aa" 整个字符串。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入：s = "aa", p = "a*"
+ * 输出：true
+ * 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：s = "ab", p = ".*"
+ * 输出：true
+ * 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= s.length <= 20
+ * 1 <= p.length <= 30
+ * s 只包含从 a-z 的小写字母。
+ * p 只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 保证每次出现字符 * 时，前面都匹配到有效的字符
+ *
+ *
+ */
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lc code=start
+class Solution
+{
+public:
+    bool isMatch(string s, string p)
+    {
+        int si = s.size();
+        int pi = p.size();
+        }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "aa"\n"a"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aa"\n"a*"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "ab"\n".*"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "mississippi"\n"mis*is*p*."\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "ssippi"\n"s*p*."\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aab"\n"c*a*b"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "ab"\n".*c"\n
+// @lcpr case=end
+
+ */
