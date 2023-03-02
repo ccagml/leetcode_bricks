36c26ed155c593f04734502104df7ee374aa14dc
ccagml <ccagml@163.com>
2023-03-02 17:40:46 +0800

    初始化的逻辑可能不对

--- 2569.更新数组后处理求和查询_20230302.cpp
+++ 2569.更新数组后处理求和查询_20230302.cpp
@@ -85,14 +85,14 @@ class segmentTree
 private:
     void pushup(int node_id)
     {
-        node_sum[node_id] = node_sum[left_right_son_id[node_id].first] + node_sum[left_right_son_id[node_id].second];
+        node_sum[node_id] = node_sum[node_id * 2] + node_sum[node_id * 2 + 1];
     }
 
     void wait_pushdown(int node_id, long long add)
     {
         // 到底了
-        int left = left_right_range[node_id].first;
-        int right = left_right_range[node_id].second;
+        int left = node_left[node_id];
+        int right = node_right[node_id];
 
         if (left == right)
         {
@@ -110,19 +110,8 @@ private:
     {
         // 需要递推
         // 继续下推
-        int cur_l = left_right_range[node_id].first;
-        int cur_r = left_right_range[node_id].second;
-        int mid = (cur_l + cur_r) >> 1;
-        if (left_right_son_id[node_id].first == -1)
-        {
-            int left_son_id = create_node(cur_l, mid);
-            left_right_son_id[node_id].first = left_son_id;
-        }
-        if (left_right_son_id[node_id].second == -1)
-        {
-            int right_son_id = create_node((mid + 1), cur_r);
-            left_right_son_id[node_id].second = right_son_id;
-        }
+        int cur_l = node_left[node_id];
+        int cur_r = node_right[node_id];
 
         // 到底了
         if (cur_l == cur_r)
@@ -131,11 +120,15 @@ private:
             node_sum[node_id] = node_val[node_id];
             return;
         }
+
+        int mid = (cur_l + cur_r) >> 1;
+        check_son(node_id * 2, cur_l, mid);
+        check_son(node_id * 2 + 1, (mid + 1), cur_r);
         long long next_add = add + node_temp_add[node_id];
         if (node_push_down_flag[node_id] || next_add > 0)
         {
-            pushdown(left_right_son_id[node_id].first, next_add);
-            pushdown(left_right_son_id[node_id].second, next_add);
+            pushdown(node_id * 2, next_add);
+            pushdown(node_id * 2 + 1, next_add);
             pushup(node_id);
             node_push_down_flag[node_id] = false;
             node_temp_add[node_id] = 0;
@@ -144,16 +137,17 @@ private:
 
     void modify(int node_id, int need_L, int need_R, long long add)
     {
-        int cur_l = left_right_range[node_id].first;
-        int cur_r = left_right_range[node_id].second;
+        int cur_l = node_left[node_id];
+        int cur_r = node_right[node_id];
 
-        node_push_down_flag[node_id] = true;
         //  cur_l   need_L  need_R  cur_r
         //  范围外
         if (cur_l > need_R || cur_r < need_L)
         {
             return;
         }
+
+        node_push_down_flag[node_id] = true;
 
         // 全包含了,不往下递推
         if (need_L <= cur_l && cur_r <= need_R)
@@ -164,43 +158,34 @@ private:
 
         // 继续下推
         int mid = (cur_l + cur_r) >> 1;
-        if (left_right_son_id[node_id].first == -1)
-        {
-            int left_son_id = create_node(cur_l, mid);
-            left_right_son_id[node_id].first = left_son_id;
-        }
-        if (left_right_son_id[node_id].second == -1)
-        {
-            int right_son_id = create_node((mid + 1), cur_r);
-            left_right_son_id[node_id].second = right_son_id;
-        }
+        int left_node_id = node_id * 2;
+        int right_node_id = node_id * 2 + 1;
+        check_son(left_node_id, cur_l, mid);
+        check_son(right_node_id, (mid + 1), cur_r);
 
-        modify(left_right_son_id[node_id].first, need_L, need_R, add);
-        modify(left_right_son_id[node_id].second, need_L, need_R, add);
-        // pushup(cur_node);
+        modify(left_node_id, need_L, need_R, add);
+        modify(right_node_id, need_L, need_R, add);
     }
 
     long long query_sum(int node_id, int NEED_L, int NEED_R)
     {
         pushdown(node_id, 0);
-        int cur_l = left_right_range[node_id].first;
-        int cur_r = left_right_range[node_id].second;
+        int cur_l = node_left[node_id];
+        int cur_r = node_right[node_id];
         if (cur_l > NEED_R || cur_r < NEED_L)
             return 0;
         if (NEED_L <= cur_l && cur_r <= NEED_R)
             return node_sum[node_id];
         // pushdown(cur_node, 0);
         long mid = (cur_l + cur_r) >> 1;
-        return query_sum(left_right_son_id[node_id].first, NEED_L, NEED_R) + query_sum(left_right_son_id[node_id].second, NEED_L, NEED_R);
+        return query_sum(node_id * 2, NEED_L, NEED_R) + query_sum(node_id * 2 + 1, NEED_L, NEED_R);
     }
 
 public:
-    vector<pair<int, int>> left_right_range;
-    vector<pair<int, int>> left_right_son_id;
-    segmentTree(int left, int right)
-    {
-        create_node(left, right);
-    }
+    vector<int> node_left;
+    vector<int> node_right;
+    int default_left_right = -1;
+    int first_id = 1;
 
     // 额外的数据
     vector<bool> node_push_down_flag;
@@ -208,26 +193,40 @@ public:
     vector<long long> node_sum;
     vector<long long> node_temp_add;
 
-    // 创建一个新的点信息
-    int create_node(int left, int right)
+    segmentTree(int need_value, int left, int right)
     {
-        left_right_range.push_back({left, right});
-        left_right_son_id.push_back({-1, -1});
-        node_push_down_flag.push_back(false);
-        node_val.push_back(0);
-        node_sum.push_back(0);
-        node_temp_add.push_back(0);
-        return left_right_range.size() - 1;
+        // 整个数的节点大概是2倍的叶子节点
+        int MX = 4 * need_value + 2;
+
+        node_left.resize(MX, default_left_right);
+        node_right.resize(MX, default_left_right);
+        node_left[first_id] = left;
+        node_right[first_id] = right;
+
+        node_push_down_flag.resize(MX, false);
+        node_val.resize(MX, 0);
+        node_sum.resize(MX, 0);
+        node_temp_add.resize(MX, 0);
+    }
+
+    // 检查初始化
+    void check_son(int node_id, int left, int right)
+    {
+        if (node_left[node_id] == default_left_right)
+        {
+            node_left[node_id] = left;
+            node_right[node_id] = right;
+        }
     }
 
     void modify(int l, int r, long long add)
     {
-        modify(0, l, r, add);
+        modify(first_id, l, r, add);
     }
 
     long long query_sum(int l, int r)
     {
-        return query_sum(0, l, r);
+        return query_sum(first_id, l, r);
     }
 };
 
@@ -236,24 +235,19 @@ class Solution
 public:
     vector<long long> handleQuery(vector<int> &nums1, vector<int> &nums2, vector<vector<int>> &queries)
     {
-
-        segmentTree *st = new segmentTree(0, nums1.size());
-        vector<int> flag_1(nums1.size() + 10);
+        segmentTree *st = new segmentTree(nums1.size(), 0, nums1.size());
         long long cur_sum = 0;
         for (int j : nums2)
         {
             cur_sum += j;
         }
 
-        // std::cout << "[";
         for (int i = 0; i < nums1.size(); i++)
         {
             if (nums1[i] == 1)
             {
                 st->modify(i, i, 1);
             }
-
-            // std::cout << nums1[i] << ",";
         }
 
         vector<long long> result;
@@ -302,6 +296,12 @@ public:
 // [277,331,331,395,395,395,515,515,615,615]	[277,331,331,445,445,445,625,625,775,775]
 // @lcpr case=start
 // [0,0,0,0,1,0,1,1,1]\n[35,29,21,34,8,48,22,43,37]\n[[1,4,7],[3,0,0],[2,27,0],[3,0,0],[1,0,3],[3,0,0],[2,6,0],[1,3,8],[2,13,0],[3,0,0],[3,0,0],[3,0,0],[2,2,0],[2,28,0],[3,0,0],[3,0,0],[2,25,0],[3,0,0],[3,0,0],[1,2,5]]\n
+// @lcpr case=end
+
+
+// [461,601,601]
+// @lcpr case=start
+//[0,0,0,0,1,0,1,0,1,1,1,0,1,1,1,1,1,0,0,0]\n[0,13,26,9,34,32,14,29,42,14,43,6,31,22,13,35,4,24,37,33]\n[[3,0,0],[2,5,0],[2,9,0],[3,0,0],[3,0,0]]
 // @lcpr case=end
 
  */
