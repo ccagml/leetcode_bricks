afc06bf62ad2f1d38bfb4774d44de5fbf5273f5c
ccagml <ccagml@163.com>
2023-04-24 16:26:22 +0800

    耗时 0:6:23

---
+++ 377.组合总和-ⅳ_20230424.cpp
@@ -0,0 +1,117 @@
+// @lcpr-before-debug-begin
+
+// @lcpr-before-debug-end
+
+/*
+ * @lc app=leetcode.cn id=377 lang=cpp
+ * @lcpr version=21907
+ *
+ * [377] 组合总和 Ⅳ
+ *
+ * https://leetcode.cn/problems/combination-sum-iv/description/
+ *
+ * algorithms
+ * Medium (52.91%)
+ * Likes:    793
+ * Dislikes: 0
+ * Total Accepted:    139.7K
+ * Total Submissions: 264.1K
+ * Testcase Example:  '[1,2,3]\n4'
+ *
+ * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
+ *
+ * 题目数据保证答案符合 32 位整数范围。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3], target = 4
+ * 输出：7
+ * 解释：
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * 请注意，顺序不同的序列被视作不同的组合。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [9], target = 3
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 1000
+ * nums 中的所有元素 互不相同
+ * 1 <= target <= 1000
+ *
+ *
+ *
+ *
+ * 进阶：如果给定的数组中含有负数会发生什么？问题会产生何种变化？如果允许负数出现，需要向题目中添加哪些限制条件？
+ *
+ */
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lc code=start
+class Solution
+{
+public:
+    int combinationSum4(vector<int> &nums, int target)
+    {
+        vector<unsigned int> vi(target + 1);
+        vi[0] = 1;
+        for (int pri = 1; pri <= target; pri++)
+        {
+            for (int c : nums)
+            {
+                if (pri >= c)
+                {
+                    vi[pri] += vi[pri - c];
+                }
+            }
+        }
+        return vi[target];
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3]\n4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300,310,320,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,480,490,500,510,520,530,540,550,560,570,580,590,600,610,620,630,640,650,660,670,680,690,700,710,720,730,740,750,760,770,780,790,800,810,820,830,840,850,860,870,880,890,900,910,920,930,940,950,960,970,980,990,111]\n999\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [9]\n3\n
+// @lcpr case=end
+
+ */
