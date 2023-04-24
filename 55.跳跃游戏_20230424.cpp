d299dcbd01a8965c8671e9779d1db0a6364325ea
ccagml <ccagml@163.com>
2023-04-24 16:14:26 +0800

    耗时 0:4:18

---
+++ 55.跳跃游戏_20230424.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=cpp
+ * @lcpr version=21907
+ *
+ * [55] 跳跃游戏
+ *
+ * https://leetcode.cn/problems/jump-game/description/
+ *
+ * algorithms
+ * Medium (43.63%)
+ * Likes:    2298
+ * Dislikes: 0
+ * Total Accepted:    697.7K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= nums.length <= 3 * 10^4
+ * 0 <= nums[i] <= 10^5
+ *
+ *
+ */
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lc code=start
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int max_v = nums[0];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i <= max_v)
+            {
+                int new_v = i + nums[i];
+                max_v = max(max_v, new_v);
+            }
+        }
+        return max_v >= nums.size() - 1;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [2,3,1,1,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,1,0,4]\n
+// @lcpr case=end
+
+ */
